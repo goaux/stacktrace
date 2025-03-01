d1de50c796a5179f62fc04ef3a72f86cb6e56b47
takumakei <takumakei@gmail.com>
2025-03-01 12:53:15 +0900

    Improve the implementation of Callers

--- v2/stacktrace.go
+++ v2/stacktrace.go
@@ -69,15 +69,16 @@ func Format(err error) string {
 // further processed using runtime.CallersFrames to obtain function details.
 func Callers(skip int) []uintptr {
 	skip += 2
-	const size = 8
+	const size = 16
 	var pc []uintptr
 	for {
 		x := make([]uintptr, size)
 		n := runtime.Callers(skip, x)
-		if n == 0 {
+		if n != len(x) {
+			pc = append(pc, x[:n]...)
 			break
 		}
-		pc = append(pc, x[:n]...)
+		pc = append(pc, x...)
 		skip += n
 	}
 	return pc
