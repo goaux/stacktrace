48bab18996e524776aa515d840b7a7f4b7ee25e4
takumakei <takumakei@gmail.com>
2025-03-09 22:39:17 +0900

    Fix docs comment

--- v2/stacktrace_go123.go
+++ v2/stacktrace_go123.go
@@ -9,8 +9,7 @@ import (
 
 // CallersFrames returns an iterator sequence of *[runtime.Frame] based on the given
 // slice of program counters (PCs). It yields stack frames one by one, stopping
-// if the iterator function returns false, if there are no more frames, or if
-// the function name is "main.main".
+// if the iterator function returns false, or if there are no more frames.
 //
 // The function utilizes runtime.CallersFrames to traverse the stack trace
 // and provides each frame through the yield function.
